38b45d6b564939e840b41a257cbf9de82ae6e451
Heejeong01110 <123rkdrkd@naver.com>
2020-12-13 13:16:25 +0900

    Revert "Merge branch 'message' into login"
    This reverts commit 5352460581f52f490c3121baee8aaf0a4e24fbfa, reversing
    changes made to b685f2d8457554553b715f1bebe8b077d63f20af.

---
+++ WebContent/index.jsp
@@ -0,0 +1,11 @@
+<%@ page language = "java" contentType = "text/html; charset = utf-8" pageEncoding = "utf-8"%>
+<html>
+<head>
+<title>게시판</title>
+</head>
+<body>
+	<script>
+		location.href = 'login.jsp';
+	</script>
+</body>
+</html>
---
+++ src/bbs/Bbs.java
@@ -0,0 +1,64 @@
+package bbs;
+
+public class Bbs {
+
+	private int bbs_id;
+	private String bbs_title;
+	private String user_id;
+	private String bbs_date;
+	private String bbs_content;
+	private int bbs_available;
+	private String bbs_type;
+	private int category_type;
+	
+
+	public int getCategory_type() {
+		return category_type;
+	}
+	public void setCategory_type(int category_type) {
+		this.category_type = category_type;
+	}
+	public int getBbs_id() {
+		return bbs_id;
+	}
+	public void setBbs_id(int bbs_id) {
+		this.bbs_id = bbs_id;
+	}
+	public String getBbs_title() {
+		return bbs_title;
+	}
+	public void setBbs_title(String bbs_title) {
+		this.bbs_title = bbs_title;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getBbs_date() {
+		return bbs_date;
+	}
+	public void setBbs_date(String bbs_date) {
+		this.bbs_date = bbs_date;
+	}
+	public String getBbs_content() {
+		return bbs_content;
+	}
+	public void setBbs_content(String bbs_content) {
+		this.bbs_content = bbs_content;
+	}
+	public int getBbs_available() {
+		return bbs_available;
+	}
+	public void setBbs_available(int bbs_available) {
+		this.bbs_available = bbs_available;
+	}
+	public String getBbs_type() {
+		return bbs_type;
+	}
+	public void setBbs_type(String bbs_type) {
+		this.bbs_type = bbs_type;
+	}
+	
+}
